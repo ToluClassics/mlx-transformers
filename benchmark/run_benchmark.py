fe4915fbec7cda3fceddd621be38889ebe8d69cc
Seun-Ajayi <oluwaseunaajayi@student.oauife.edu.ng>
2024-07-11 23:29:06 +0100

    fix ruff

--- benchmark/run_benchmark.py
+++ benchmark/run_benchmark.py
@@ -18,7 +18,8 @@ from transformers import (
     XLMRobertaModel,
 )
 
-import os, sys
+import os 
+import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from src.mlx_transformers.models import BertForMaskedLM as MlxBertForMaskedLM
